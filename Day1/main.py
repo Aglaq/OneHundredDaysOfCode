4b3b29b8280edd4a2f26a28681eb5466ace233cc
Adam Z <adamzubel@gmail.com>
2025-11-10 07:51:30 -0800

    Day 1 - final project

--- Day1/main.py
+++ Day1/main.py
@@ -1,2 +1,20 @@
-print("Hello, Day 1!")
-print("Hello, Day 1!")
+# Day 1 - Working with Variables
+# Day 1 - Project: Band Name Generator
+
+# Greeting
+print("Welcome to the Band Name Generator.") 
+# Creating two variables, asking for city and pet's name
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+# Combining into one string, saving in variable and printing on the screen
+band_name = print("Your band name could be " + city + " " + pet)
+
+
+# Examples and exercises from Day 1
+# print("Hello World!")
+# print("Hello World!\nHello World!")
+# print("Hello " + "Adam")
+# print("Hello " + input("What is your name?") + "!")
+# username = input("What is your name?")
+# length = len(username)
+# print(length)
