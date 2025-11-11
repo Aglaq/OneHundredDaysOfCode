9718dc764ab254b2fa2a2c3dee5bbfef92617534
Adam Z <adamzubel@gmail.com>
2025-11-11 05:13:37 -0800

    Day2 Final Version

--- Day2/main.py
+++ Day2/main.py
@@ -2,12 +2,12 @@
 # Day 2 - Project: Tip Calculator
 
 # Greeting
-print("Welcome to the tip calculator")
+print("Welcome to the Tip Calculator!")
 # Asking for bill in $ and for tip in % and number of payers
 bill = input("What was the total bill? $")
-tip = input("How much tip would you like to give? %")
-payers = input("How many people do split the bill")
+tip = input("How much tip would you like to give? 10, 12, or 15 ")
+payers = input("How many people do split the bill? ")
 # Calculating amount per person 
-amount_to_pay = round(((bill * (1 + tip / 100)) / payers), 2)
+amount_to_pay = round(((float(bill) * (1 + int(tip) / 100)) / int(payers)), 2)
 # Printing result
-print(f"Each person should pay: {amount_to_pay}")
+print(f"Each person should pay: ${amount_to_pay:.2f}")
