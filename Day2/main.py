4c857e3503735c69cdec9511b25bb8b8d92c2bfb
Adam Z <adamzubel@gmail.com>
2025-11-11 04:39:34 -0800

    Day2 beginning of project

--- Day2/main.py
+++ Day2/main.py
@@ -1,1 +1,6 @@
-print("Hello, Day 2!")
+# Day 2 - Understanding Data Types and How to Manipulate Strings
+# Day 2 - Project: Tip Calculator
+
+#Greeting
+print("Welcome to the tip calculator")
+print(input("What was the total bill? $"))
