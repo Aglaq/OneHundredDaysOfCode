b76abdad2c25a1a624a6b1f5a0ace50a61886fa7
Adam Z <adamzubel@gmail.com>
2025-11-11 05:02:43 -0800

    Day2 Tip Calculator

--- Day2/main.py
+++ Day2/main.py
@@ -1,6 +1,13 @@
 # Day 2 - Understanding Data Types and How to Manipulate Strings
 # Day 2 - Project: Tip Calculator
 
-#Greeting
+# Greeting
 print("Welcome to the tip calculator")
-print(input("What was the total bill? $"))
+# Asking for bill in $ and for tip in % and number of payers
+bill = input("What was the total bill? $")
+tip = input("How much tip would you like to give? %")
+payers = input("How many people do split the bill")
+# Calculating amount per person 
+amount_to_pay = round(((bill * (1 + tip / 100)) / payers), 2)
+# Printing result
+print(f"Each person should pay: {amount_to_pay}")
