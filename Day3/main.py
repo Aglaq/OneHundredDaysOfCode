7cafd741b51611f9fb58ea6ebe91b9db60594264
Adam Z <adamzubel@gmail.com>
2025-11-13 03:16:25 -0800

    Day3 Game - story

--- Day3/main.py
+++ Day3/main.py
@@ -22,7 +22,7 @@ print('''
                         \,---_|    |_---./
                         ooOO(_)    (_)OOoo
 ''')
-print("One of more of your adventures brought you to magical, calm forest. After walking for few hours, you got really tired and goes to sleep")
+print("One of more of your adventures brought you to the magical, calm forest. After walking for few hours, you got really tired and goes to sleep")
 print("Suddenly, terrible roar wakes you up. You open your eyes and see big ogre with a mallet in his hand looking at you like hungry man looks at the sandwich.")
 print("Without hesitation you start to run, but you don't remember from where you have come. You run straight by path.")
 direction = input("The path divides into two. To turn left and go to the river type 'left', to turn right and go deeper to the forest type 'right'.\n")
@@ -32,12 +32,29 @@ if direction == "left" or direction == "Left":
     river_crossing = input("To go through bridge type 'bridge', to take a boat type 'boat'.\n")
     if river_crossing == "bridge" or river_crossing == "Bridge":
         print("Without any more hesitation you took your chance with a bridge. You managed to jump over each hole and reach the other side. The ogre is swimming after you")
+        print("You still aren't safe. You have few minutes to hide and wait for monster to go away. There is field of wheat with the big tree, scarecrow and few bundles of hay.")
+        print("You look around and see some places to hide.")
+        hiding_spot = input("To climb very tall tree and hide between it's leaves type 'tree'. To take old cloth from scarecrow, dress as one and hide on the field type 'scarecrow'. To hide in one of bundles of hay type 'hay'.\n")
+        if hiding_spot == "scarecrow" or hiding_spot == "Scarecrow":
+            print("You took old rags and stand on the fiels pretending to be scarecrow. Ogre came, checked every bundle of hay.")
+            print("You weren't found. You survived. You waited few more minutes just to be sure. You reached old house in the end of the field. Farmer showed you way home.")
+            print("Congratulations, YOU WON!!!")
+        elif hiding_spot == "tree" or hiding_spot == "Tree":
+            print("You reach the tree and starts climbing. Unfortunatelly, one of the branches broke under your weight. You felt right into big hands of the ogre.")
+            print("GAME OVER")
+        elif hiding_spot == "hay" or hiding_spot == "Hay":
+            print("You jump into biggest bundle of hay and hold your breath to not make any sound. What were you thinking? That's the first place where ogre looked. You've been found.")
+            print("GAME OVER")
+        else:
+            print("error - unrecognized hiding spot")
     elif river_crossing == "boat" or river_crossing == "Boat":
-        print("You reached the boat and noticed that it's full of holes, too late to go through bridge now. Ogre caught you! GAME OVER")
+        print("You reached the boat and noticed that it's full of holes, too late to go through bridge now. Ogre caught you!")
+        print("GAME OVER")
     else:
         print("error - wrong crossing method")
 elif direction == "right" or direction == "Right":
-    print("You turned right and run deeper into the forest, maybe you have lost the ogr, but you never found way home. GAME OVER")
+    print("You turned right and run deeper into the forest, maybe you have lost the ogre, but you never found way home.")
+    print("GAME OVER")
 else:
     print("error - direction not recognized")
 
