0ca20c2cf335dc2794b72c340a08baeee193937e
Adam Z <adamzubel@gmail.com>
2025-11-14 04:35:02 -0800

    Day4 Rock Paper Scissors finished

--- Day4/main.py
+++ Day4/main.py
@@ -3,15 +3,43 @@
 import random
 import hand_gestures
 
+computers_hand = random.randint(0, 2)
+
 print("Welcome to Rock Paper Scissors")
 players_hand = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
 
 if players_hand == 0:
+    print("You chose ROCK")
     print(hand_gestures.rock)
 elif players_hand == 1:
+    print("You chose PAPER")
     print(hand_gestures.paper)
 elif players_hand == 2:
+    print("You chose SCISSORS")
     print(hand_gestures.scissors)
+else:
+    print("You didn't choose properly")
+
+computers_hand = random.randint(0, 2)
+
+if computers_hand == 0:
+    print("Computer chose ROCK")
+    print(hand_gestures.rock)
+elif computers_hand == 1:
+    print("Computer chose PAPER")
+    print(hand_gestures.paper)
+elif computers_hand == 2:
+    print("Computer chose SCISSORS")
+    print(hand_gestures.scissors)
+
+if computers_hand == players_hand:
+    print("Tie!")
+elif (computers_hand == 0 and players_hand == 1) or (computers_hand == 1 and players_hand == 2) or (computers_hand == 2 and players_hand == 0):
+    print("You win!")
+elif (computers_hand == 0 and players_hand == 2) or (computers_hand == 1 and players_hand == 0) or (computers_hand == 2 and players_hand == 1):
+    print("You lose!")
+else:
+    print("error")
 
 # Day4 examples and exercises
 # number = random.randint(1, 10)
