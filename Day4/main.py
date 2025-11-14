a524677a5e542bbfe1080782ddccbb4c7608ba7b
Adam Z <adamzubel@gmail.com>
2025-11-13 17:07:24 -0800

    Day4 Heads and Tails

---
+++ Day4/main.py
@@ -0,0 +1,16 @@
+# Day 4 - Randomisation and Python Lists
+# Day 4 - Project: Rock Paper Scissors - Game
+import random
+
+# print("Hello, Day 4!")
+# number = random.randint(1, 10)
+# print(number)
+
+# random_number_0_to_1 = random.random()
+# print(random_number_0_to_1)
+
+coin_toss = random.randint(0, 1)
+if coin_toss == 1:
+    print("Heads")
+else:
+    print("Tails")
