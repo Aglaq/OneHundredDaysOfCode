3c5042ca249b80680b9c140029031fe321be0a61
Adam Z <adamzubel@gmail.com>
2025-11-20 12:13:46 -0800

    Day 6 - final project

--- Day6/main.py
+++ Day6/main.py
@@ -1,1 +1,23 @@
-print("Hello, Day 6!")
+# Day 6 - Functions, Code Blocks and While Loops
+# Day 6 - Project: Escaping the Maze
+
+# Whole Project was done on the website: https://reeborg.ca
+# Below code is for exercise on the above webpage, world: Maze
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while front_is_clear():
+#     move()
+# turn_left()
+    
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
