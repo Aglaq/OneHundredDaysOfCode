ad0c5e832d22f00786a0c122955920b61a6b0a52
Adam Z <adamzubel@gmail.com>
2025-11-21 08:31:05 -0800

    Day 7 - step4 losing

--- Day7/main.py
+++ Day7/main.py
@@ -2,7 +2,10 @@
 # Day 7 - Project: Hangman
 
 import random
+import art_hangman
 
+game_over = False
+lives = 6
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -11,11 +14,11 @@ for n in range(len(chosen_word)):
     placeholder += "_"
 print(placeholder)
 
-game_over = False
 while not game_over:
     display = ""
     guess = input("Guess a letter: ").lower()
     # print(guess)
+    
     for n, letter in enumerate(chosen_word):
         if letter == guess:
             # print("Right")
@@ -27,7 +30,14 @@ while not game_over:
             # print("Wrong")
             display += "_"
     print(display)
+    if placeholder == display:
+        lives -= 1
+        print(lives)
     placeholder = display
+    print(art_hangman.hangman_stages[lives])
+    if lives == 0:
+        game_over = True
+        print("You lost!")
     # print(placeholder)
     if "_" not in placeholder:
         game_over = True
