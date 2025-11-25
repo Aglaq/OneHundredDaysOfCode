882c5cd024e98aa51067f5b9fd785c8696b7ada6
Adam Z <adamzubel@gmail.com>
2025-11-25 15:24:04 -0800

    Day 8 caesar final version

--- Day8/main.py
+++ Day8/main.py
@@ -3,16 +3,21 @@
 import string
 import logo
 
+
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+# Variable go keep while going
 repetition = True
 
+# Printing logo from logo.py
 print(logo.logo)
 while repetition:
 
+    #user's input
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
+    # Main function
     def caesar(encode_or_decode, original_text, shift_amount):
         final_text = ""
 
