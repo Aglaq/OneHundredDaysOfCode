b2c3d18b979f14308458b3c9b0732ec26a6bb113
Adam Z <adamzubel@gmail.com>
2025-11-25 13:38:13 -0800

    Day 8 caesar part3 start

--- Day8/main.py
+++ Day8/main.py
@@ -1,9 +1,12 @@
 # Day 8 - Functions with Inputs
 # Day 8 - Project: Caesar Cipher
+import logo
+
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 repetition = True
 
+print(logo.logo)
 while repetition:
 
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
