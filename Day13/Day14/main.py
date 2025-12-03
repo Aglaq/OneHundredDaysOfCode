4232df25aa04a3a50a45feede238b9f0973e01f1
Adam Z <adamzubel@gmail.com>
2025-12-03 12:56:49 -0800

    Day 13 finished

---
+++ Day13/Day14/main.py
@@ -0,0 +1,4 @@
+# Day 14 - Higher or Lower
+# Day 14 - Project: Higher or Lower Game
+
+print("Hello, Day 14!")
--- Day13/main.py
+++ Day13/main.py
@@ -1,17 +1,19 @@
 # Day 13 - Debugging
-# Day 13 - Project
-import random
+# Day 13 - Project: no project
+# import random
 
-# print("Hello, Day 13!")
+# # print("Hello, Day 13!")
 
-def mutate(a_list):
-    b_list = []
-    new_item = 0
-    for item in a_list:
-        new_item = item * 2
-        new_item += random.randint(1, 3)
-        new_item = new_item + item
-        b_list.append(new_item)
-    print(b_list)
+# def mutate(a_list):
+#     b_list = []
+#     new_item = 0
+#     for item in a_list:
+#         new_item = item * 2
+#         new_item += random.randint(1, 3)
+#         new_item = new_item + item
+#         b_list.append(new_item)
+#     print(b_list)
 
-mutate([1, 2, 4, 7, 12, 13])
+# mutate([1, 2, 4, 7, 12, 13])
+
+# Most exercises from this day were just debugging code using different method. Debugger, Print, think like computer etc
