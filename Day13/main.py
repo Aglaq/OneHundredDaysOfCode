fd30e6a76e39039e59cecbeb22fbf4d15348e093
Adam Z <adamzubel@gmail.com>
2025-12-03 12:54:29 -0800

    Day 13 finished

--- Day13/main.py
+++ Day13/main.py
@@ -1,1 +1,17 @@
-print("Hello, Day 13!")
+# Day 13 - Debugging
+# Day 13 - Project
+import random
+
+# print("Hello, Day 13!")
+
+def mutate(a_list):
+    b_list = []
+    new_item = 0
+    for item in a_list:
+        new_item = item * 2
+        new_item += random.randint(1, 3)
+        new_item = new_item + item
+        b_list.append(new_item)
+    print(b_list)
+
+mutate([1, 2, 4, 7, 12, 13])
