6b892d173862eb7d5356d2892423710cc543c76e
Adam Z <adamzubel@gmail.com>
2025-12-08 09:32:09 -0800

    Day 14 final version

--- Day14/main.py
+++ Day14/main.py
@@ -8,11 +8,11 @@ import game_data
 # print(game_data.data)
 
 def random_item():
-    '''Function that chooses random list from game_data.'''
+    '''Choooses random person from game_data.'''
     return random.choice(game_data.data)
 
 def comparison(list_one, list_two):
-    '''Function that compares which person has more followers and returns result.'''
+    '''Compares which person has more followers and returns result.'''
     followers_A = item_A["follower_count"]
     followers_B = item_B["follower_count"]
     # print(followers_A)
@@ -20,7 +20,7 @@ def comparison(list_one, list_two):
     bigger = 0
     if followers_A > followers_B:
         bigger = "A"
-    elif followers_A < followers_B:
+    else:
         bigger = "B"
     return bigger
 
@@ -28,28 +28,29 @@ game = True
 points = 0
 item_A = random_item()
 item_B = random_item()
+if item_A == item_B:
+    item_B = random_item()
 
 print("\n" * 20)
 print(art.logo)
 while game == True:
-    print(f"Compare A: {item_A["name"]}, {item_A["profession"]}, from {item_A["country"]}.")
+    print(f"Compare A: {item_A["name"]}, a {item_A["profession"]}, from {item_A["country"]}.")
     print("")
     print(art.vs)
     print("")
-    print(f"Against B: {item_B["name"]}, {item_B["profession"]}, from {item_B["country"]}.")
+    print(f"Against B: {item_B["name"]}, a {item_B["profession"]}, from {item_B["country"]}.")
     players_choice = input("Who has more followers? Type 'A' or 'B': ").capitalize()
-    # print(players_choice)
+    while players_choice not in ("A", "B"):
+        players_choice = input("Not recognized symbol. Type 'A' or 'B': ").capitalize()
+    print("\n" * 20)
+    print(art.logo)
 
     result = comparison(item_A, item_B)
     if players_choice == result:
         points += 1
         item_A = item_B
         item_B = random_item()
-        print("\n" * 20)
-        print(art.logo)
         print(f"You're right! Current score: {points}.")
     else:
-        print("\n" * 20)
-        print(art.logo)
         print(f"Sorry, that's wrong. Final score: {points}")
         game = False
