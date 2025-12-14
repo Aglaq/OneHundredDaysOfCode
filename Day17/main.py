11e64a6f64c9e08b2b1dc4977d7b8ceb9a765eac
Adam Z <adamzubel@gmail.com>
2025-12-14 07:02:55 -0800

    Day 17 quiz - final version

--- Day17/main.py
+++ Day17/main.py
@@ -1,4 +1,18 @@
 # Day 17 - the Benefits of OOP
 # Day 17 - Project: The Quiz
 
-print("Hello from Day 17!")
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for item in question_data:
+    new_question = Question(item["text"], item["answer"])
+    question_bank.append(new_question)
+
+new_quiz = QuizBrain(question_bank)
+
+while new_quiz.still_has_questions():
+    new_quiz.next_question()
+
+new_quiz.end_of_the_game()
