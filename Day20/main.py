1b3550337a84f53eaa4685125de44d0a1b1d9d76
Adam Z <adamzubel@gmail.com>
2025-12-18 14:49:15 -0800

    Day 20 part1

--- Day20/main.py
+++ Day20/main.py
@@ -1,4 +1,18 @@
 # Day 20 - Build the Snake Game Part 1: Animation and Coordinates
 # Day 20 - Project: Snake Game
+from turtle import Screen, Turtle
 
-print("Hello from Day 20!")
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+for i in range(3):
+    new_snake_part = Turtle("square")
+    new_snake_part.up()
+    new_snake_part.color("white")
+    new_snake_part.goto((0 + (-20) * i), 0)
+
+
+
+screen.exitonclick()
