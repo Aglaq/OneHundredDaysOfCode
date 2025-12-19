a55fe535273ee4a16a2fa297e7cc47ca53593e81
Adam Z <adamzubel@gmail.com>
2025-12-19 08:09:50 -0800

    Day 21 exercises

--- Day21/main.py
+++ Day21/main.py
@@ -1,1 +1,21 @@
-print("Hello from Day 21!")
+# Day 21 - Build the Snake Game Part 2: Inheritance & List Slicing
+# Day 21 - Project: Snake Game
+
+# Examples and exercises from day 21
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def swim(self):
+        print("Moving in water.")
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
