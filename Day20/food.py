d2ee9838ac13b3eabf7c2147d6ced80dcc12c041
Adam Z <adamzubel@gmail.com>
2025-12-20 04:43:29 -0800

    Day 21 collision with food

---
+++ Day20/food.py
@@ -0,0 +1,17 @@
+import random
+from turtle import Turtle
+
+class Food(Turtle):
+
+    def __init__(self, shape = "circle", undobuffersize = 1000, visible = True):
+        super().__init__(shape, undobuffersize, visible)
+        self.penup()
+        self.shapesize(stretch_len = 0.5, stretch_wid = 0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
