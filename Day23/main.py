cbf7287a38f3489d8fb763fe94ad9256b25c9402
Adam Z <adamzubel@gmail.com>
2025-12-23 15:02:49 -0800

    Day 23 starting code

---
+++ Day23/main.py
@@ -0,0 +1,17 @@
+# Day 23 - Capstone Project
+# Day 23 - The Turtle Crossing
+
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
