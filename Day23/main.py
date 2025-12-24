aac53e4e7949381136dab6fb2d4152ba487fede9
Adam Z <adamzubel@gmail.com>
2025-12-24 05:58:10 -0800

    Day 23 player

--- Day23/main.py
+++ Day23/main.py
@@ -11,7 +11,21 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
 game_is_on = True
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    if player.ycor() == player.finish_line:
+        player.restart()
+        # scoreboard.level_up()
+        # car_manager.level_up()
+
+
+
+screen.exitonclick()
