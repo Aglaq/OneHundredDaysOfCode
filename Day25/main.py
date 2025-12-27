65f41738d24369b944a01c1130f02949447cbd40
Adam Z <adamzubel@gmail.com>
2025-12-27 07:56:24 -0800

    Day 25 States Game - part1

--- Day25/main.py
+++ Day25/main.py
@@ -1,13 +1,35 @@
 # Day 25 - Working with CSV Data and the Pandas Library
 # Day 25 - Project: Name the States
-import pandas
+import turtle
 
+screen = turtle.Screen()
+screen.title("States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
 
+turtle.shape(image)
 
-
-
+screen.exitonclick()
 
 # Examples and exercises Day 25
+# import pandas
+
+# squirrels_data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# colors = squirrels_data["Primary Fur Color"]
+# gray = colors.str.count("Gray")
+# red = colors.str.count("Cinnamon")
+# black = colors.str.count("Black")
+# sum_of_gray = gray.sum(min_count = 1)
+# sum_of_red = red.sum(min_count = 1)
+# sum_of_black = black.sum(min_count = 1)
+
+# final_dict = {
+#     "Fur Color": ["grey", "red", "black"],
+#     "Count": [sum_of_gray, sum_of_red, sum_of_black],
+# }
+# final_table = pandas.DataFrame(final_dict)
+# final_table.to_csv("squirrel_count.csv")
+
 # data = pandas.read_csv("weather_data.csv")
 # print(data)
 # print(data["temp"])
