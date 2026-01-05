cbb3c3ebf8456a03442812532add83a9a646aec1
Adam Z <adamzubel@gmail.com>
2026-01-05 13:47:52 -0800

    Day 26 nato project - final

--- Day26/main.py
+++ Day26/main.py
@@ -1,4 +1,33 @@
 # Day 26 - List Comprehension
 # Day 26 - Project: NATO Alphabet
+import pandas
 
-print("Hello from Day 26!")
+#TODO 1. Create a dictionary in this format:
+nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_alphabet_dict = {row.letter:row.code for (index, row) in nato_data_frame.iterrows()}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_word = input("Enter a word: ").upper()
+user_word_final = [nato_alphabet_dict[code] for code in user_word]
+print(user_word_final)
+
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"], 
+#     "score": [56, 76, 98]
+# }
+
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+
+# student_data_frame = pandas.DataFrame(student_dict)
+
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
