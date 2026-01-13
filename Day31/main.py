eb44eaab6ea70651abd21bc8fe4e7ec60884f38b
Adam Z <adamzubel@gmail.com>
2026-01-13 09:21:39 -0800

    Day 31 Flash Card App - GUI

--- Day31/main.py
+++ Day31/main.py
@@ -1,4 +1,31 @@
 # Day 31 - Capstone Project
 # Day 31 - Project: Flash Card App
+from tkinter import *
 
-print("Hello from Day 31!")
+#----------------------------CONSTANTS-------------------------------
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+#-------------------------------GUI----------------------------------
+
+window = Tk()
+window.title("Flash Cards")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# Canvas
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(column=0, row=0, columnspan=2)
+language_text = canvas.create_text(400, 150, text="Title", fill="black", font=("Ariel", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="word", fill="black", font=("Ariel", 60, "bold"))
+
+# Buttons
+right_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_image, bg=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR)
+right_button.grid(column=1, row=1)
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR)
+wrong_button.grid(column=0, row=1)
+
+window.mainloop()
