6cc29e7fd23221cb3eee2a76aa47baf5f67f18b1
Adam Z <adamzubel@gmail.com>
2026-01-13 02:58:37 -0800

    Day 30 final

--- Day30/main.py
+++ Day30/main.py
@@ -1,4 +1,26 @@
 # Day 30 - Errors, Exceptions and JSON Data
-# Day 30 - Project: Improving the Password
+# Day 30 - Project: Improving the Password Manager
 
-print("Hello from Day 30!")
+# The Project of this day was upgrading Password Manager from Day 29
+
+# Examples Day 30
+
+# height = float(input("Height: "))
+# weight = int(input("Weight: "))
+
+# if height > 3:
+#     raise ValueError("Human Height should not be over 3 meters.")
+
+# bmi = weight / height ** 2
+# print(bmi)
+
+
+# try:
+#     file = open("a_file.txt")
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# else:
+#     file.read()
+# finally:
+#     file.close()
