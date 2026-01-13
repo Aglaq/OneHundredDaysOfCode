1021f0fc731b39ed639f819610bef72acaa5b614
Adam Z <adamzubel@gmail.com>
2026-01-13 13:07:35 -0800

    Day 31 Creating Random Card

--- Day31/main.py
+++ Day31/main.py
@@ -1,10 +1,26 @@
 # Day 31 - Capstone Project
 # Day 31 - Project: Flash Card App
 from tkinter import *
-
+import pandas
+import random
 #----------------------------CONSTANTS-------------------------------
 
 BACKGROUND_COLOR = "#B1DDC6"
+LANG_1 = "French"
+LANG_2 = "English"
+
+#----------------------RANDOM_CARD--------------------------
+
+# Reading CSV file with pandas
+words_list_data = pandas.read_csv("data/french_words.csv")
+# Changing DataFrame to list of dictionaries
+words_list = [{f: e} for f, e in zip(words_list_data[LANG_1], words_list_data[LANG_2])]
+
+def choosing_random_card():
+    random_card = random.choice(words_list)
+    random_word = (list(random_card.keys())[0])
+    canvas.itemconfig(language_text, text=LANG_1)
+    canvas.itemconfig(word_text, text=random_word)
 
 #-------------------------------GUI----------------------------------
 
@@ -22,10 +38,12 @@ word_text = canvas.create_text(400, 263, text="word", fill="black", font=("Ariel
 
 # Buttons
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, bg=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR)
+right_button = Button(image=right_image, bg=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR, command=choosing_random_card)
 right_button.grid(column=1, row=1)
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR)
+wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR, command=choosing_random_card)
 wrong_button.grid(column=0, row=1)
+
+choosing_random_card()
 
 window.mainloop()
