cda9fdbf0f9f5016793fb02900ebde9429ac4955
Adam Z <adamzubel@gmail.com>
2026-01-29 03:27:10 -0800

    Day 32 Automated Birthday Wisher - start

--- Day32/main.py
+++ Day32/main.py
@@ -1,33 +1,38 @@
 # Day 32 - Email SMTP and the datetime module
 # Day 32 - Project: Automated Birthday Wisher
 
-import datetime as dt
-import smtplib
-import random
-
-my_email = "testing101days@gmail.com"
-password = "bkmhzgrgfldcvybk"
-
-# Finding day of the week
-now = dt.datetime.now()
-day_of_week = now.weekday()
 
 
-if day_of_week == 3:
-    # Opening txt file and changing to list
-    with open("quotes.txt") as quotes:
-        list_of_quotes = (quotes.readlines())
 
-    # Choosing random quote
-    random_quote = random.choice(list_of_quotes)
 
-    # Sending email
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email, 
-                            to_addrs="adamzubel@gmail.com", 
-                            msg=f"Subject:Quote of the week!\n\n {random_quote}")
+# # Quote of the week
+# import datetime as dt
+# import smtplib
+# import random
+
+# my_email = ___
+# password = ___
+
+# # Finding day of the week
+# now = dt.datetime.now()
+# day_of_week = now.weekday()
+
+
+# if day_of_week == 3:
+#     # Opening txt file and changing to list
+#     with open("quotes.txt") as quotes:
+#         list_of_quotes = (quotes.readlines())
+
+#     # Choosing random quote
+#     random_quote = random.choice(list_of_quotes)
+
+#     # Sending email
+#     with smtplib.SMTP("smtp.gmail.com") as connection:
+#         connection.starttls()
+#         connection.login(user=my_email, password=password)
+#         connection.sendmail(from_addr=my_email, 
+#                             to_addrs="___", 
+#                             msg=f"Subject:Quote of the week!\n\n {random_quote}")
 
 # import smtplib
 
