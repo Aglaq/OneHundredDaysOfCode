763657e9883695bfe211afab1a5396729f14f7fd
Adam Z <adamzubel@gmail.com>
2026-01-30 07:19:30 -0800

    Day 32 Automated Birthday Wisher checking date and choosing random letter

--- Day32/main.py
+++ Day32/main.py
@@ -1,7 +1,46 @@
 # Day 32 - Email SMTP and the datetime module
 # Day 32 - Project: Automated Birthday Wisher
+import datetime as dt
+import pandas
+import random
 
+##################### Extra Hard Starting Project ######################
 
+# 1. Update the birthdays.csv
+birthdays = pandas.read_csv("birthdays.csv")
+new_birthday = input("Do you want to add new Birthday? Type 'y' for yes or 'n' for no.\n").lower()
+if new_birthday == "y":
+    new_name = input("Give name:\n")
+    new_email = input("Give email address:\n")
+    new_year = input("Give year:\n")
+    new_month = input("Give month:\n")
+    new_day = input("Give day:\n")
+    birthdays.loc[len(birthdays)] = [new_name, new_email, new_year, new_month, new_day]
+
+birthdays.to_csv("birthdays.csv", index=False)
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# Finding todays month and day
+now = dt.datetime.now()
+month_now = now.month
+day_now = now.day
+
+birthday_month = birthdays[birthdays.month == month_now]
+birthday_day = birthday_month[birthday_month.day == day_now]
+name = birthday_day.at[0, "name"]
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+if not birthday_day.empty:
+    letter_number = random.randint(1, 3)
+    with open(f"./letter_templates/letter_{letter_number}.txt") as random_letter:
+        letter = (random_letter.readlines())
+        letter[0] = letter[0].replace("[NAME]", f"{name}")
+    for line in letter:
+        with open(f"./letter_templates/final_letter.txt", mode="a") as final_letter:
+            final_letter.write(line)
+
+# 4. Send the letter generated in step 3 to that person's email address.
 
 
 
