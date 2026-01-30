73e52ba5c7cfdd0a67109eb6b7698e91ee857223
Adam Z <adamzubel@gmail.com>
2026-01-30 08:35:10 -0800

    Day 32 Automated Birthday Wisher - final

--- Day32/main.py
+++ Day32/main.py
@@ -3,35 +3,39 @@
 import datetime as dt
 import pandas
 import random
+import smtplib
+
+my_email = "___"
+password = "___"
 
 ##################### Extra Hard Starting Project ######################
 
 # 1. Update the birthdays.csv
 birthdays = pandas.read_csv("birthdays.csv")
-new_birthday = input("Do you want to add new Birthday? Type 'y' for yes or 'n' for no.\n").lower()
-if new_birthday == "y":
-    new_name = input("Give name:\n")
-    new_email = input("Give email address:\n")
-    new_year = input("Give year:\n")
-    new_month = input("Give month:\n")
-    new_day = input("Give day:\n")
-    birthdays.loc[len(birthdays)] = [new_name, new_email, new_year, new_month, new_day]
+# new_birthday = input("Do you want to add new Birthday? Type 'y' for yes or 'n' for no.\n").lower()
+# if new_birthday == "y":
+#     new_name = input("Give name:\n")
+#     new_email = input("Give email address:\n")
+#     new_year = input("Give year:\n")
+#     new_month = input("Give month(1-12):\n")
+#     new_day = input("Give day(1-31):\n")
+#     birthdays.loc[len(birthdays)] = [new_name, new_email, new_year, new_month, new_day]
 
-birthdays.to_csv("birthdays.csv", index=False)
+# birthdays.to_csv("birthdays.csv", index=False)
 
 # 2. Check if today matches a birthday in the birthdays.csv
-
 # Finding todays month and day
 now = dt.datetime.now()
 month_now = now.month
 day_now = now.day
 
-birthday_month = birthdays[birthdays.month == month_now]
-birthday_day = birthday_month[birthday_month.day == day_now]
-name = birthday_day.at[0, "name"]
+df_birthday_month = birthdays[birthdays.month == month_now]
+df_birthday_day = df_birthday_month[df_birthday_month.day == day_now]
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-if not birthday_day.empty:
+if not df_birthday_day.empty:
+    name = df_birthday_day.at[0, "name"]
+    email = df_birthday_day.at[0, "email"]
     letter_number = random.randint(1, 3)
     with open(f"./letter_templates/letter_{letter_number}.txt") as random_letter:
         letter = (random_letter.readlines())
@@ -41,6 +45,19 @@ if not birthday_day.empty:
             final_letter.write(line)
 
 # 4. Send the letter generated in step 3 to that person's email address.
+    with open(f"./letter_templates/final_letter.txt", mode="r") as file:
+        msg = file.read()
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, 
+                            to_addrs=email, 
+                            msg=f"Subject: Happy Birthday!\n\n{msg}")
+
+    # Cleaning final_letter.txt
+    with open(f"./letter_templates/final_letter.txt", mode="w") as clean:
+        clean.write("")
 
 
 
@@ -75,14 +92,14 @@ if not birthday_day.empty:
 
 # import smtplib
 
-# my_email = "testing101days@gmail.com"
-# password = "bkmhzgrgfldcvybk"
+# my_email = "___"
+# password = "___"
 
 # with smtplib.SMTP("smtp.gmail.com") as connection:
 #     connection.starttls()
 #     connection.login(user=my_email, password=password)
 #     connection.sendmail(from_addr=my_email, 
-#                         to_addrs="adamzubel@gmail.com", 
+#                         to_addrs="___", 
 #                         msg="Subject:Hello\n\n This is body of the email!")
 
 # import datetime as dt
