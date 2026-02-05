e7fa4aae203b1219d7507950efc6287b4cf342a7
Adam Z <adamzubel@gmail.com>
2026-02-05 02:55:55 -0800

    Day 33 quotes

--- Day33/main.py
+++ Day33/main.py
@@ -1,2 +1,12 @@
 # Day 33 - Application Programming Interfaces
 # Day 33 - Project: ISS Overhead Notifier
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+iss_position = (longitude, latitude)
+print(iss_position)
