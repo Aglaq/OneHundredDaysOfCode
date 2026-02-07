09a725febc96295bd83b92e465b535fb89d57363
Adam Z <adamzubel@gmail.com>
2026-02-07 16:49:33 +0200

    Day 34 taking data from web

--- Day34/main.py
+++ Day34/main.py
@@ -1,4 +1,23 @@
 # Day 34 - API
 # Day 34 - Project: The Quizzler App
 
-print("Hello from Day 34!")
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
