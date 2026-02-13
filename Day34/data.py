30c570113d0f9fc7b968dc1aa05cb4b304b8e061
Adam Z <adamzubel@gmail.com>
2026-02-13 12:53:10 +0200

    Day 34 GUI

--- Day34/data.py
+++ Day34/data.py
@@ -11,7 +11,6 @@ parameters = {
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 question_data = response.json()["results"]
-print(question_data)
 # question_data = [
 #     {
 #         "category": "Science: Computers",
--- Day34/ui.py
+++ Day34/ui.py
@@ -1,2 +1,34 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="Question", fill=THEME_COLOR, font= ("Ariel", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, bg=THEME_COLOR, highlightbackground=THEME_COLOR)
+        self.true_button.grid(column=0, row=2)
+        false_image = PhotoImage(file="images/false.png")
+        self.true_button = Button(image=false_image, bg=THEME_COLOR, highlightbackground=THEME_COLOR)
+        self.true_button.grid(column=1, row=2)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
