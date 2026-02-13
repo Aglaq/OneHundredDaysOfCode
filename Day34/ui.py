346136f06c5c024fd471d1c0141125a45b7aa664
Adam Z <adamzubel@gmail.com>
2026-02-13 17:33:33 +0200

    Day 34 final version

--- Day34/ui.py
+++ Day34/ui.py
@@ -22,19 +22,33 @@ class QuizInterface:
         self.true_button = Button(image=true_image, bg=THEME_COLOR, highlightbackground=THEME_COLOR, command=self.true_answer)
         self.true_button.grid(column=0, row=2)
         false_image = PhotoImage(file="images/false.png")
-        self.true_button = Button(image=false_image, bg=THEME_COLOR, highlightbackground=THEME_COLOR, command=self.false_answer)
-        self.true_button.grid(column=1, row=2)
+        self.false_button = Button(image=false_image, bg=THEME_COLOR, highlightbackground=THEME_COLOR, command=self.false_answer)
+        self.false_button.grid(column=1, row=2)
 
         self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.configure(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_answer(self):
-        self.quiz.check_answer("True")
+        self.give_feedback(self.quiz.check_answer("True"))
 
     def false_answer(self):
-        self.quiz.check_answer("False")
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.configure(bg="green")
+        else:
+            self.canvas.configure(bg="red")
+        self.window.after(1000, self.get_next_question)
