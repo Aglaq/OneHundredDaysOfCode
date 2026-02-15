5f940c88a72cba8a92dbda7de27e4175a7191222
Adam Z <adamzubel@gmail.com>
2026-02-15 17:43:04 +0200

    Day 35 will it rain

--- Day35/main.py
+++ Day35/main.py
@@ -1,1 +1,23 @@
-print("Hello from Day 35!")
+# Day 35 - API Keys, Authentication, Environment Variables, and Sending SMS
+# Day 35 - Rain Alert Application
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/foreast"
+parameters = {
+    "lat": 42.497681,
+    "lon": 27.470030,
+    "appid": "636fdcf3988faba51b5bf1d6af12f5c6",
+    "cnt": 4,
+}
+
+response = requests.get(url=OWM_Endpoint, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella.")
