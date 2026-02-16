9e14f59ee44d4819b6918feddcd3c171750e5d12
Adam Z <adamzubel@gmail.com>
2026-02-16 19:52:50 +0200

    Day 36 finished

--- Day36/main.py
+++ Day36/main.py
@@ -28,51 +28,41 @@ news_params = {
     "apiKey": news_api, 
 }
 
-# response_stock = requests.get(url=alpha_endpoint, params=alpha_params)
-# response_stock.raise_for_status()
-# stock_data = response_stock.json()["Time Series (Daily)"]
+response_stock = requests.get(url=alpha_endpoint, params=alpha_params)
+response_stock.raise_for_status()
+stock_data = response_stock.json()["Time Series (Daily)"]
 
-# values = list(stock_data.values())
+values = list(stock_data.values())
 
-# if len(values) >= 2:
-#     yesterday_stock, before_yesterday_stock = values[0]["4. close"], values[1]["4. close"]
+if len(values) >= 2:
+    yesterday_stock, before_yesterday_stock = values[0]["4. close"], values[1]["4. close"]
 
-# if float(before_yesterday_stock) <= 0.95 * float(yesterday_stock) or float(before_yesterday_stock) >= 1.05 * float(yesterday_stock):
-response_news = requests.get(url=news_endpoint, params=news_params)
-response_news.raise_for_status()
-news_data = response_news.json()
+percent_difference = round((float(yesterday_stock) - float(before_yesterday_stock))/float(yesterday_stock) * 100)
 
-articles = []
+if percent_difference <= -5 or percent_difference >= 5:
+    response_news = requests.get(url=news_endpoint, params=news_params)
+    response_news.raise_for_status()
+    news_data = response_news.json()
 
-for i in range(3):
-    article = {
-        "headline": news_data["articles"][i]["title"],
-        "brief": news_data["articles"][i]["description"]
-    }
-    articles.append(article)
+    articles = []
 
-final_message = f"{STOCK}: \n"
-for article in articles:
-    final_message += f"Headline: {article['headline']}\n"
-    final_message += f"Brief: {article['brief']}\n"
+    for i in range(3):
+        article = {
+            "headline": news_data["articles"][i]["title"],
+            "brief": news_data["articles"][i]["description"]
+        }
+        articles.append(article)
 
-# client = Client(account_sid, auth_token)
-# message = client.message \
-#     .create(
-#         body=final_message
-#         from="..."
-#         to="..."
-#     )
-# print(message.status)
+    final_message = f"{STOCK}: {percent_difference}%\n"
+    for article in articles:
+        final_message += f"Headline: {article['headline']}\n"
+        final_message += f"Brief: {article['brief']}\n"
 
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    client = Client(account_sid, auth_token)
+    message = client.message \
+        .create(
+            body=final_message
+            from="..."
+            to="..."
+        )
+    print(message.status)
