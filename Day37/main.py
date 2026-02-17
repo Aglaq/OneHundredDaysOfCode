7e67ca2eb322d880d6d39929d0ed1c9c9f8194ac
Adam Z <adamzubel@gmail.com>
2026-02-17 15:41:56 +0200

    Day 37 pixela

--- Day37/main.py
+++ Day37/main.py
@@ -1,1 +1,13 @@
-print("Hello from Day 37!")
+# Day 37 - Advanced Authentication and POST / PUT / DELETE Requests
+# Day 37 - Project: Habit Tracking
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": "xkfivy368snchfr2",
+    "username": "Aglaq",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+requests.post(url=pixela_endpoint, json=)
